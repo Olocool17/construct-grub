e8ba118712268cbb963831ba2b010dd4f245ef9f
Olocool17 <22843298+Olocool17@users.noreply.github.com>
2022-04-17 00:42:27 +0200

    Add consistent spacing

--- install.sh
+++ install.sh
@@ -77,11 +77,12 @@ else
     construct_echo "Exiting..."
 fi
 
-construct_echo "Setting terminal background..."
+construct_echo "Setting GRUB terminal background..."
 sudo sed -i "\|^export theme|a background_image /${GRUB_DIR}/themes/${THEME}/background.png" ${GRUB_CFG}
 
 construct_echo "Renaming Windows boot entry (if applicable)..."
 sudo sed -i "s|'Windows Boot Manager[^']*|'Windows 10|g" ${GRUB_CFG}
+
 construct_echo "Renaming EUFI Firmware boot entry (if applicable)..."
 sudo sed -i "s|'UEFI Firmware Settings'|'UEFI Settings' --class efi|g" ${GRUB_CFG}
 
@@ -89,9 +90,9 @@ if [[ "$ID" =~ arch ]]
 then
     construct_echo "Deleting Arch with fallback initrams entry..."
     sudo awk -i inplace "/Arch Linux, with Linux linux \(/,/}/ {next} {print}" ${GRUB_CFG}
+
     construct_echo "Renaming EUFI Firmware boot entry (if applicable)..."
     sudo sed -i "s|'Arch Linux, with Linux linux'|'Arch Linux'|g" ${GRUB_CFG}
-    
 fi
 
 construct_echo "Adding shutdown menu entry..."
